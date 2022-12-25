666b789ca300e863480b686addaf7a41743885fd
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2022-12-25 17:16:49 +0200

    mod-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function galleryMarkup(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </div>`
+    )
+    .join("");
+}
+
+gallery.innerHTML = galleryMarkup(galleryItems);
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src = "${e.target.dataset.source}" width = "800" height = "600">
+    `);
+  instance.show();
+
+  gallery.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+});
